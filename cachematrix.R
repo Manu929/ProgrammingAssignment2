d7348dc29f214a192f5a6fa9f5331769a2c1f994
Manu928 <Manu928@users.noreply.github.com>
2020-04-12 16:09:38 +0200

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,41 @@
-## Put comments here that give an overall description of what your
-## functions do
+## calculate and cache the inverse of a matrix with first function (result not returned),
+## then get the inverse of the matrix by either getting it 
+## from the cache (parent environment) if calculated before, or by calculating it now
 
-## Write a short comment describing this function
+## makeCacheMatrix function with x=matrix defined in same environment as makeCacheMatrix (not within this function itself)(empty matrix as default to avoid error)
+## s initialized as object within same environment as makeCacheMatrix, used later
+## define 'getters and setters' with set: assign the input y to x in parent environment, and NULL to s (to clear cache in case of earlier use of cacheSolve)
+## get (with x defined in and got from parent environment), setsolve (applies solve function and assigns input to s in parent environment),
+## and getsolve (similar as 'get' for x) and save it as list with names (names needed for second function cacheSolve)
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        s <- NULL
+        set <- function(y) {
+                x <<- y
+                s <<- NULL
+        }
+        get <- function() x
+        setsolve <- function(solve) s <<- solve(x)
+        getsolve <- function() solve(x)
+        list(set = set, get = get,
+             setsolve = setsolve,
+             getsolve = getsolve)
 }
 
 
-## Write a short comment describing this function
+## return inverse of matrix x (by function solve).
+## if value stored in parent environment (cached), then get it from there and return it
+## if not (s is NULL), then calculate it new, get data of matrix x,
+## apply the function solve()to the data, assign the result to s and print s
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        s <- x$getsolve()
+        if(!is.null(s)){
+                message ("getting cached data")
+                return (s)
+        }
+        data <- x$get()
+        s <- solve (data,...)
+        x$setsolve(s)
+        s
 }
